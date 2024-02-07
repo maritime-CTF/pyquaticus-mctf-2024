cf08cd7dc0326b062b188b004f26d99e446efb94
john-kliem <john.kliem@nrl.navylmil>
2024-02-07 14:57:53 -0500

    Updated example zip folder use 3 agents

--- pyquaticus/envs/pyquaticus.py
+++ pyquaticus/envs/pyquaticus.py
@@ -141,8 +141,8 @@ class PyQuaticusEnvBase(ParallelEnv, ABC):
                             action_dict[player.id] += 'X'
                         elif action_dict[player.id] not in ['SC', 'CC', 'PC']:
                             action_dict[player.id] = action_dict[player.id][:-1]
-                    _, heading = mag_bearng_to(player.pos, self.config_dict["aquaticus_field_points"][action_dict[player.id]], player.heading)
-                    if -1 <= self.get_distance_between_2_points(player.pos, self.config_dict["aquaticus_field_points"][action_dict[player.id]]) <= 1: #
+                    _, heading = mag_bearing_to(player.pos, self.config_dict["aquaticus_field_points"][action_dict[player.id]], player.heading)
+                    if -2 <= self.get_distance_between_2_points(player.pos, self.config_dict["aquaticus_field_points"][action_dict[player.id]]) <= 2: #
                         speed = 0.0
                     else:
                         speed = self.max_speed
